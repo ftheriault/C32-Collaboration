4cdd3615c2614d1f60f0a847c244e3754bbd760f
Frédéric Thériault <fred.theriault@gmail.com>
2026-04-21 07:43:08 -0400

    Réinitialisation

--- main.cpp
+++ main.cpp
@@ -10,32 +10,18 @@ int count = 0;
  * Fonction du prof!
 */
 void fct00() {
-    cout << "     ,.----------.     \n";
-    cout << "    ((|          |     \n";
-    cout << "   .--\\          /--.  \n";
-    cout << "  '._  '========'  _.' \n";
-    cout << "     `""""""""""""`    \n";
-    cout << "L'orange est le meilleur ami de l'informaticien !!\n";
 }
 
 void fct01() {
-
 }
 
 void fct02() {
-	cout<< "CAVE EMPT\n";
 }
 
 void fct03() {
-	cout << "Hello groupe ! :)\n";
 }
 
 void fct04() {
-	cout <<"(MEOW)\n";
-	cout <<"  v\n";
-	cout <<"/\\_/\\\n";
-	cout <<"( o.o )\n";
-	cout <<"> ^ <\n";
 }
 
 void fct05() {
@@ -43,7 +29,6 @@ void fct05() {
 
 void fct06() {
 }
-
 
 void fct07() {
 }
@@ -58,54 +43,36 @@ void fct10() {
 }
 
 void fct11() {
-     cout << "Salut comment ca va!!";
 }
 
 void fct12() {
-cout << ("Salut git !");
 }
 
 void fct13() {
-	cout << ".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.\n";
-	cout << "Je sais aussi, dit Candide, qu'il faut cultiver notre jardin.\n";
-	cout << ".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.\n";
 }
 
 void fct14() {
-cout << "O       o O       o O       o \n ";
-cout << "| O   o | | O   o | | O   o | \n ";
-cout << "| | O | | | | O | | | | O | | \n " ;
-cout << "| o   O | | o   O | | o   O | \n ";
-cout << "o       O o       O o       O \n ";}
+}
 
 void fct15() {
 }
 
 void fct16() {
-	// Quote d'un sans-abris...
-	cout << "Vous m'avez l'air princier... Vous m'en voyez ravi!";
-	cout << "Test";
 }
 
 void fct17() {
-for(int i = 0; i<=9; i++){
-	cout << "Amine Fanid\n";
-}
 }
 
 void fct18() {
-	cout << "voici mon code qui compile";
 }
 
 void fct19() {
 }
 
 void fct20() {
-cout << "boi";
 }
 
 void fct21() {
-cout << "Le soleil me manque";
 }
 
 void fct22 () {
@@ -130,23 +97,18 @@ void fct28() {
 }
 
 void fct29() {
-	cout << "beignes\n";
 }
 
 void fct30() {
-	cout << "Hello";
 }
 
 void separator() {
-	cout << "\n======= fct " << count++ << " ===============\n";
 }
 
 void fct31() {
-	cout << "Amenez moi des chocolats avec praline noisettes";
 }
 
 void fct32() {
-	cout << "J'aime les chats!\n";
 }
 
 int main(int argc, char* argv[])
